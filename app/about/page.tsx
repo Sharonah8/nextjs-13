4844dd20959d59616bd81cee6269d027d5c98658
Snyamongo <Snyamongo@safaricom.co.ke>
2023-07-21 09:33:42 +0300

    Add link to the home page from the about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import Link from "next/link";
 
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <>
+      <h1>About</h1>
+      <Link rel="stylesheet" href="/">
+        Link to the Home Page
+      </Link>
+    </>
+  );
 }
